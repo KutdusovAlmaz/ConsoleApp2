bb7fc394feeca3cd0a2ce56eaf18f89dbbdc1405
user <user@WIN-B9Q80GRCCEO>
2024-04-28 21:47:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите расстояние в сантиметрах: ");
+        int L = Convert.ToInt32(Console.ReadLine());
+
+        int meters = L / 100;
+
+        Console.WriteLine("Количество полных метров: " + meters);
+    }
+}
+
